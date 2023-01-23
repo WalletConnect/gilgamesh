16d20873456f14a6f22ec037efdc2583ebaea3ad
Derek <alexanderderekrein@gmail.com>
2023-01-24 00:13:18 +0400

    feat: change error message

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,6 @@ async fn main() -> Result<()> {
     let (_signal, shutdown) = broadcast::channel(1);
     dotenv().ok();
 
-    let config = Configuration::new().expect("Failed to load config!");
+    let config = Configuration::new().expect("Failed to load config :(");
     gilgamesh::bootstap(shutdown, config).await
 }
