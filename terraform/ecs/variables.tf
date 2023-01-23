4bfcca03b3e4dd96472e5915489170423cf5a87b
Derek <alexanderderekrein@gmail.com>
2023-01-24 00:05:15 +0400

    feat: deploy service

---
+++ terraform/ecs/variables.tf
@@ -0,0 +1,56 @@
+variable "region" {
+  type = string
+}
+
+variable "app_name" {
+  type = string
+}
+
+variable "image" {
+  type = string
+}
+
+variable "database_url" {
+  type      = string
+  sensitive = true
+}
+
+variable "prometheus_endpoint" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "route53_zone_id" {
+  type = string
+}
+
+variable "fqdn" {
+  type = string
+}
+
+variable "acm_certificate_arn" {
+  type = string
+}
+
+variable "public_subnets" {
+  type = set(string)
+}
+
+variable "private_subnets" {
+  type = set(string)
+}
+
+variable "cpu" {
+  type = number
+}
+
+variable "memory" {
+  type = number
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 locals {
-  app_name            = "gilgamesh"
+  app_name            = "history"
   fqdn                = terraform.workspace == "prod" ? var.public_url : "${terraform.workspace}.${var.public_url}"
   latest_release_name = data.github_release.latest_release.name
   version             = coalesce(var.image_version, substr(local.latest_release_name, 1, length(local.latest_release_name)))
@@ -11,7 +11,7 @@ data "assert_test" "workspace" {
 }
 
 data "github_release" "latest_release" {
-  repository  = local.app_name
+  repository  = "gilgamesh"
   owner       = "walletconnect"
   retrieve_by = "latest"
 }
@@ -23,6 +23,61 @@ module "tags" {
   env         = terraform.workspace
 }
 
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  name   = "${terraform.workspace}-${local.app_name}"
+
+  cidr = "10.0.0.0/16"
+
+  azs             = var.azs
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+  private_subnet_tags = {
+    Visibility = "private"
+  }
+  public_subnet_tags = {
+    Visibility = "public"
+  }
+
+  enable_dns_support     = true
+  enable_dns_hostnames   = true
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
+}
+
+# TODO: Remove this later
+module "database_cluster" {
+  source = "terraform-aws-modules/rds-aurora/aws"
+
+  name           = "${terraform.workspace}-${local.app_name}-database"
+  engine         = "aurora-postgresql"
+  engine_version = "13.6"
+  engine_mode    = "provisioned"
+  instance_class = "db.serverless"
+  instances = {
+    1 = {}
+  }
+
+  database_name = "postgres"
+
+  vpc_id  = module.vpc.vpc_id
+  subnets = module.vpc.private_subnets
+
+  allowed_cidr_blocks = [module.vpc.vpc_cidr_block]
+
+  storage_encrypted = true
+  apply_immediately = true
+
+  allow_major_version_upgrade = true
+
+  serverlessv2_scaling_configuration = {
+    min_capacity = 2
+    max_capacity = 10
+  }
+}
+
 module "dns" {
   source = "github.com/WalletConnect/terraform-modules.git//modules/dns"
 
@@ -32,4 +87,35 @@ module "dns" {
 
 resource "aws_prometheus_workspace" "prometheus" {
   alias = "prometheus-${terraform.workspace}-${local.app_name}"
+}
+
+data "aws_ecr_repository" "repository" {
+  name = "gilgamesh"
+}
+
+module "ecs" {
+  source = "./ecs"
+
+  app_name            = "${terraform.workspace}-${local.app_name}"
+  prometheus_endpoint = aws_prometheus_workspace.prometheus.prometheus_endpoint
+  database_url        = "postgres://${module.database_cluster.cluster_master_username}:${module.database_cluster.cluster_master_password}@${module.database_cluster.cluster_endpoint}:${module.database_cluster.cluster_port}/postgres"
+  image               = "${data.aws_ecr_repository.repository.repository_url}:${local.version}"
+  acm_certificate_arn = module.dns.certificate_arn
+  cpu                 = 512
+  fqdn                = local.fqdn
+  memory              = 1024
+  private_subnets     = module.vpc.private_subnets
+  public_subnets      = module.vpc.public_subnets
+  region              = var.region
+  route53_zone_id     = module.dns.zone_id
+  vpc_cidr            = module.vpc.vpc_cidr_block
+  vpc_id              = module.vpc.vpc_id
+}
+
+module "o11y" {
+  source = "./monitoring"
+
+  environment = terraform.workspace
+  app_name = local.app_name
+  prometheus_workspace_id = aws_prometheus_workspace.prometheus.id
 }
