11841b3e0ed7bca8ec3c55cf7b14b9f7ebc6cc72
Xavier Basty <xavier@walletconnect.com>
2023-03-29 16:33:29 +0200

    fix: urls for deploy environments (#12)

---
+++ terraform/outputs.tf
@@ -0,0 +1,13 @@
+output "fqdn" {
+  description: "The FQDN of the deployed instance"
+  value = local.fqdn
+}
+
+output version {
+  description: "The version of the deployed instance"
+  value = local.version
+}
+
+output "" {
+  value = ""
+}
