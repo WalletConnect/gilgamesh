d3c509f8fb8d9cd68f2f580902fa344ac0f94072
Xavier Basty <xavier@walletconnect.com>
2023-03-31 15:10:42 +0200

    fix: environment in monitoring module (#20)

--- terraform/monitoring/main.tf
+++ terraform/monitoring/main.tf
@@ -7,29 +7,6 @@ locals {
   #  )
 }
 
-resource "grafana_data_source" "prometheus" {
-  type = "prometheus"
-  name = "${module.this.id}-amp"
-  url  = "https://aps-workspaces.${module.this.environment}.amazonaws.com/workspaces/${var.prometheus_workspace_id}/"
-
-  json_data_encoded = jsonencode({
-    httpMethod    = "GET"
-    manageAlerts  = false
-    sigV4Auth     = true
-    sigV4AuthType = "ec2_iam_role"
-    sigV4Region   = module.this.environment
-  })
-}
-
-resource "grafana_data_source" "cloudwatch" {
-  type = "cloudwatch"
-  name = "${module.this.id}-cloudwatch"
-
-  json_data_encoded = jsonencode({
-    defaultRegion = module.this.environment
-  })
-}
-
 data "jsonnet_file" "dashboard" {
   source = "${path.module}/dashboard.jsonnet"
 
