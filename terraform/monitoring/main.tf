e910faacc3559a6b265bdf3b169a4f01e3da1cf6
Rakowskiii <maciejrak@protonmail.com>
2023-04-03 16:27:39 +0900

    fix: formatting

--- terraform/monitoring/main.tf
+++ terraform/monitoring/main.tf
@@ -2,7 +2,7 @@ locals {
   opsgenie_notification_channel = "l_iaPw6nk"
   notifications = (
     var.environment == "prod" ?
-    [{"uid": "${local.opsgenie_notification_channel}"}] :
+    [{ "uid" : "${local.opsgenie_notification_channel}" }] :
     []
   )
 }
